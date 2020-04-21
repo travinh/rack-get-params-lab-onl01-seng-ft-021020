3ce2aa261a7f192ce9563d1812677759a8a54c0f
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-04-21 07:47:49 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,7 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
+  @@cart =[]
 
   def call(env)
     resp = Rack::Response.new
@@ -10,6 +11,12 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
+    elsif req.path.match(/cart/)
+      @@cart.each do |item|
+        resp.write "#{item}\n"
+      end
+    elsif req.path.match(/add/)
+      search_term = req.params[""]
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
